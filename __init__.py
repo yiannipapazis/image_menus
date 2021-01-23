8aeeb1a2b1b4e987477e769ba376deb27ff9efc6
Yianni <yianni.papazis@gmail.com>
2021-01-24 01:28:30 +1030

    make active now working. just have to figure parent menu with multiple material  slots

--- __init__.py
+++ __init__.py
@@ -13,7 +13,6 @@
 
 import bpy
 
-
 bl_info = {
     "name" : "Image Operators",
     "author" : "Yianni Papazis",
@@ -25,10 +24,11 @@ bl_info = {
     "category" : "Generic"
 }
 
+addon_keymaps = []
 
 class reload_images_menu(bpy.types.Menu):
     bl_idname = "reload_images_menu"
-    bl_label = "Reload Image"
+    bl_label = "Reload Images"
     bl_space_type = "3D_VIEW"
 
     def draw(self, context):
@@ -37,20 +37,20 @@ class reload_images_menu(bpy.types.Menu):
         layout.label(text="Images")
         layout.label(text="Reload All", icon="FILE_REFRESH")
         image_list = get_images_from_objects()
-        for image in image_list:
+        for image_node in image_list:
+            image = image_node.image
             o = layout.operator(reload_image_by_name.bl_idname, text=image.name, icon='IMAGE_DATA')
             o.image_name = image.name
 
 class reload_image_by_name(bpy.types.Operator):
     bl_idname = "object.reload_image_by_name"
-    bl_label = "reload_image_by_name"
+    bl_label = "Reload Image by Name"
     image_name = bpy.props.StringProperty()
 
     def execute(self, context):
         if self.image_name:
             
             bpy.data.images[self.image_name].reload()
-            print("hello")
         return {'FINISHED'}
 
 class load_from_selected_menu(bpy.types.Menu):
@@ -61,30 +61,104 @@ class load_from_selected_menu(bpy.types.Menu):
     def draw(self, context):
         layout = self.layout
 
-        test_list = ['image1', 'image2', 'image3']
+        image_list = get_images_from_objects()
 
-        for image in test_list:
+        for image in image_list:
             layout.label(text=image, icon='IMAGE_DATA')
 
+class make_active_from_selected_menu(bpy.types.Menu):
+    bl_idname = "object.make_active_from_selected_menu"
+    bl_label = "Make Image Active"
+    image_name = ""
+
+    class menu(bpy.types.Menu):
+        bl_idname = "object.make_active_from_image_menu"
+        bl_label = "Material"
+        mat_name = bpy.props.StringProperty()
+
+        def draw(self, context):
+            layout = self.layout
+            mat_image_nodes = look_for_images_from_mat(bpy.data.materials[self.mat_name])
+            operators = []
+            if mat_image_nodes:
+                for image_node in mat_image_nodes:
+                    o = layout.operator(self.operator.bl_idname, text=image_node.image.name, icon='IMAGE_DATA')
+                    o.mat_name = self.mat_name
+                    o.node_name = str(image_node)
+            else:
+                layout.label(text="No images")
+
+        class operator(bpy.types.Operator):
+            bl_idname = "object.make_active_from_selected"
+            bl_label = "Make Image Active"
+            mat_name = bpy.props.StringProperty()
+            node_name = bpy.props.StringProperty()
+
+            def execute(self, context):
+
+                node_tree = bpy.data.materials[self.mat_name].node_tree
+                # TODO find a cleaner way of getting out node
+                for each in node_tree.nodes:
+                    if str(each) == self.node_name:
+                        node = each
+                node.select = True
+                node_tree.nodes.active = node
+                return {'FINISHED'}
+    
+    def draw(self, context):
+        layout = self.layout
+        material_list = get_materials_from_selected()
+        if material_list:
+            for mat in material_list:
+                m = self.menu
+                m.mat_name = mat
+                layout.menu(m.bl_idname, text=mat, icon="MATERIAL")
+        else:
+            layout.label(text='No materials')
+
 def get_images_from_objects():
-    images = []
     checked_mats = []
+    images = []
     active_object = bpy.context.active_object
     materials = active_object.material_slots
     for mat in materials:
         mat = mat.material
         if mat not in checked_mats:
-            for node in mat.node_tree.nodes:
-                if node.type == 'TEX_IMAGE' and node.image not in images:
-                    images.append(node.image)
+            checked_mats.append(mat)
+            found_images = look_for_images_from_mat(mat)
+            for img in found_images:
+                if img not in images:
+                    images.append(img)
     return images
+
+def look_for_images_from_mat(mat = bpy.types.Material):
+    image_list = []
+    for node in mat.node_tree.nodes:
+        if node.type == 'TEX_IMAGE' and node.image not in image_list:
+            image_list.append(node)
+    return image_list
+
+def get_materials_from_selected():
+    object_materials = []
+    checked_mats = []
+    active_object = bpy.context.active_object
+    if active_object:
+        materials = active_object.material_slots
+        for mat in materials:
+            mat = mat.name
+            if mat not in checked_mats:
+                checked_mats.append(mat)
+                object_materials.append(mat)
+    return object_materials
 
 classes = (
     reload_images_menu,
     load_from_selected_menu,
-    reload_image_by_name
+    reload_image_by_name,
+    make_active_from_selected_menu.menu.operator,
+    make_active_from_selected_menu,
+    make_active_from_selected_menu.menu
 )
-
 
 def register():
 
@@ -94,6 +168,7 @@ def register():
     # register keymaps
     wm = bpy.context.window_manager
     kc = wm.keyconfigs.addon
+
     km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
     kmi = km.keymap_items.new(
         "wm.call_menu",
@@ -102,16 +177,35 @@ def register():
         shift=True
         )
     kmi.properties.name = reload_images_menu.bl_idname
+    addon_keymaps.append((km,kmi))
 
     km = kc.keymaps.new(name="Image", space_type="IMAGE_EDITOR")
     kmi = km.keymap_items.new(
         "wm.call_menu",
-        type="L",
+        type="I",
         value="PRESS",
         shift=True
         )
     kmi.properties.name = load_from_selected_menu.bl_idname
-def unregister():
-    bpy.utils.unregister_class(reload_images_menu)
+    addon_keymaps.append((km,kmi))
 
-    # TODO unregister keymaps
+    km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
+    kmi = km.keymap_items.new(
+        "wm.call_menu",
+        type="I",
+        value="PRESS",
+        shift=True
+    )
+    kmi.properties.name = make_active_from_selected_menu.bl_idname
+    addon_keymaps.append((km,kmi))
+
+def unregister():
+
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+
+    # Unregister keymaps
+    for km, kmi in addon_keymaps:
+        km.keymap_items.remove(kmi)
+    
+    addon_keymaps.clear()
